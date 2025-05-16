0ce21c943d59a81b9d17b35666f4637904f093c2
Yash Dubey <44433103+Y-dub@users.noreply.github.com>
2025-05-16 14:39:59 +0100

    Add files via upload

---
+++ src/data/generated/bib.js
@@ -0,0 +1,97 @@
+﻿define({ entries : {
+    "adnan2020wsi": {
+        "author": "Adnan, M. and Kalra, S. and others",
+        "doi": "10.48550/arXiv.2004.07399",
+        "journal": "arXiv preprint arXiv:2004.07399",
+        "keywords": "GNN, Patch Graph, TCGA",
+        "title": "Representation Learning of Histopathology Images using Graph Neural Networks",
+        "type": "article",
+        "year": "2020"
+    },
+    "brussee2024gnnsurvey": {
+        "author": "Brussee, L. and Sahu, S. and Mahapatra, D.",
+        "doi": "10.48550/arXiv.2406.12808",
+        "journal": "arXiv preprint arXiv:2406.12808",
+        "keywords": "Survey, GNN, Histopathology",
+        "title": "Graph Neural Networks in Histopathology: Emerging Trends and Future Directions",
+        "type": "article",
+        "year": "2024"
+    },
+    "carloni2023causalsignals": {
+        "author": "Carloni, E. and Colantonio, S.",
+        "doi": "10.48550/arXiv.2309.10399",
+        "journal": "arXiv preprint arXiv:2309.10399",
+        "keywords": "Causal Signals, CNN, Robustness",
+        "title": "Exploiting Causality Signals in Medical Images: A Pilot Study with Empirical Results",
+        "type": "article",
+        "year": "2023"
+    },
+    "castro2020causality": {
+        "author": "Castro, D. C. and Walker, I. and Glocker, B.",
+        "doi": "10.1038/s41467-020-17478-w",
+        "journal": "Nature Communications",
+        "keywords": "Causal Inference, Distribution Shift, Fairness",
+        "pages": "3673",
+        "title": "Causality Matters in Medical Imaging: A Perspective",
+        "type": "article",
+        "volume": "11",
+        "year": "2020"
+    },
+    "chan2023heterognn": {
+        "author": "Chan, T. H. and Tsang, Y. and others",
+        "booktitle": "Proceedings of the IEEE Conference on Computer Vision and Pattern Recognition (CVPR)",
+        "doi": "https://openaccess.thecvf.com/content/CVPR2023/html/Chan_Histopathology_Whole_Slide_Image_Analysis_With_Heterogeneous_Graph_Representation_Learning_CVPR_2023_paper.html",
+        "keywords": "Heterogeneous GNN, Breast Cancer, WSI",
+        "title": "Histopathology Whole Slide Image Analysis With Heterogeneous Graph Representation Learning",
+        "type": "inproceedings",
+        "year": "2023"
+    },
+    "kiani2024ai": {
+        "author": "Kiani, A. and Esteva, A. and others",
+        "doi": "10.1038/s41746-024-01106-8",
+        "journal": "npj Digital Medicine",
+        "keywords": "Meta-Analysis, Digital Pathology, AI in Healthcare",
+        "title": "Artificial Intelligence in Digital Pathology: A Systematic Review and Meta-Analysis",
+        "type": "article",
+        "year": "2024"
+    },
+    "lu2022causal": {
+        "author": "Lu, Y.",
+        "doi": "10.48550/arXiv.2210.16034",
+        "journal": "arXiv preprint arXiv:2210.16034",
+        "keywords": "Causality, Survey, Representation Learning",
+        "title": "A Survey on Causal Representation Learning and Future Work for Medical Image Analysis",
+        "type": "article",
+        "year": "2022"
+    },
+    "xu2022topognn": {
+        "author": "Xu, Y. and Wang, Y. and others",
+        "doi": "10.1186/s12859-022-05063-5",
+        "journal": "BMC Bioinformatics",
+        "keywords": "Topology, Survival Analysis, Breast Cancer",
+        "number": "1",
+        "pages": "148",
+        "title": "A Graph Neural Network Framework for Mapping Histological Topology in Whole Slide Images",
+        "type": "article",
+        "volume": "23",
+        "year": "2022"
+    },
+    "yu2024attention": {
+        "author": "Yu, L. and Gupta, R. and others",
+        "doi": "10.48550/arXiv.2407.02484",
+        "journal": "arXiv preprint arXiv:2407.02484",
+        "keywords": "Attention, Interpretability, WSI",
+        "title": "Characterizing the Interpretability of Attention Maps in Digital Pathology",
+        "type": "article",
+        "year": "2024"
+    },
+    "zhou2022review": {
+        "author": "Zhou, L. and Bai, W. and others",
+        "doi": "https://www.melba-journal.org/2022:028",
+        "journal": "Medical Imaging and Learning Benchmark Archive (MELBA)",
+        "keywords": "Causality, Medical Imaging, Fairness",
+        "title": "A Review of Causality for Learning Algorithms in Medical Image Analysis",
+        "type": "article",
+        "year": "2022"
+    }
+}});
